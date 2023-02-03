3c0316ff8a8c2973cb1c9e6209acd8b4c957d634
Fede Trucco <fedetrucco_03@hotmail.com>
2023-02-03 18:06:49 -0300

    03/02/2023

--- ManosObra/biblioteca-2/src/main/resources/static/js/toggle.js
+++ ManosObra/biblioteca-2/src/main/resources/static/js/toggle.js
@@ -1,40 +1,14 @@
-var toggle = document.getElementById('darkmode');
-var body = document.querySelector('body');
-var nav = document.getElementById('navbar');
-var list1 = document.getElementById('lista1');
-var list2 = document.getElementById('lista2');
-var list3 = document.getElementById('lista3');
-var list4 = document.getElementById('lista4');
-var list5 = document.getElementById('lista5');
-var main0 = document.querySelector('main0');
-var main = document.querySelector('main');
-var main2 = document.querySelector('main2');
-var main3 = document.querySelector('main3');
-var footer = document.querySelector('footer');
-var sky = document.querySelector('sky');
+const toggleButton = document.querySelector("#dark-mode-toggle");
 
+//Obtiene el estado actual del modo oscuro desde localStorage
+const currentDarkModeSetting = localStorage.getItem("darkMode");
 
-toggle.onclick = function () {
-  toggle.classList.toggle('active');
-  body.classList.toggle('active');
-  nav.classList.toggle('active');
-  list1.classList.toggle('active');
-  list2.classList.toggle('active');
-  list3.classList.toggle('active');
-  list4.classList.toggle('active');
-  list5.classList.toggle('active');
-  main.classList.toggle('active');
-  main2.classList.toggle('active');
-  main3.classList.toggle('active');
-  footer.classList.toggle('active');
-  sky.classList.toggle('active');
-}
+//Establece el estado actual del modo oscuro en la página
+document.body.classList.toggle("dark-mode", currentDarkModeSetting === "enabled");
 
-  /*if (document.body.classList.add('dark')){
-
-  document.body.classList.contains('dark');
-  document.body.classList.remove('dark');
-
-  }else {
-    document.body.classList.add('dark');
-  }*/
+//Escucha el clic en el botón y alterna el estado del modo oscuro
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem("darkMode", document.body.classList.contains("dark-mode") ? "enabled" : "disabled");
+});
+    
